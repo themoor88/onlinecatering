ffc68d06ff6879c6ca18bac807223669b9d55ad3
WajidKagzi <wjkagzi@gmail.com>
2015-12-12 18:37:55 -0500

    Modifying production.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '54.201.198.52', user: 'deploy', roles: %w{web app db}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
