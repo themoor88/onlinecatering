72bf2790c7335c6a1ef0ce0802dd866860543e6e
WajidKagzi <wjkagzi@gmail.com>
2015-12-23 23:59:03 -0500

    Added devise routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :caterers
+  devise_for :users, controllers: {
+    registrations: 'users/custom_devise/registrations'
+  }
+  devise_for :caterers, controllers: {
+    registrations: 'caterers/custom_devise/registrations'
+  }
   root 'pages#home'
   post '/subscribe' => 'pages#subscribe_email'
 end
