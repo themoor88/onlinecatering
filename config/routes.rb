9df38c9ccfb3da111a066a9cc78d4d76ca9576e3
WajidKagzi <wjkagzi@gmail.com>
2016-01-02 23:39:04 -0500

    Spacing.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/custom_devise/registrations'
   }
+
   devise_for :caterers, controllers: {
     registrations: 'caterers/custom_devise/registrations'
   }
+
   root 'pages#home'
   post '/subscribe' => 'pages#subscribe_email'
 end
