d910b89f488248517bd3e48175a5ad68837aeb42
WajidKagzi <wjkagzi@gmail.com>
2016-01-03 22:44:03 -0500

    Adding relevant routes for caterers and users.

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,19 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   post '/subscribe' => 'pages#subscribe_email'
+
+  devise_scope :user do
+
+    namespace :users do
+      get '/profile' => 'dashboard#profile', as: 'profile'
+      resources :orders
+    end
+  end
+
+  devise_scope :caterer do
+    namespace :caterers do
+      get '/profile' => 'dashboard#profile', as: 'profile'
+      resources :food_items
+    end
+  end
 end
