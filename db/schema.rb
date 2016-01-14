91d4acc9d4a700d04f98aa1b5be31f8c273b57ff
WajidKagzi <wjkagzi@gmail.com>
2016-01-13 23:51:34 -0500

    Generated order and food item models.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110230157) do
+ActiveRecord::Schema.define(version: 20160114045108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,8 +34,18 @@ ActiveRecord::Schema.define(version: 20160110230157) do
   add_index "caterers", ["email"], name: "index_caterers_on_email", unique: true, using: :btree
   add_index "caterers", ["reset_password_token"], name: "index_caterers_on_reset_password_token", unique: true, using: :btree
 
+  create_table "food_items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "lists", force: :cascade do |t|
     t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
